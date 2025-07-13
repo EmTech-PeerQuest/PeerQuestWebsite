8429b5d915ac2eaf141656360430b62fd56b67d9
Jenel <jenelesteron01@gmail.com>
2025-07-13 23:00:34 +0800

    depl

---
+++ PeerQuestFrontEnd/app/about/About.tsx
@@ -0,0 +1,11 @@
+
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <h1>About PeerQuest</h1>
+      <p>This is the About page for PeerQuest. Update this content as needed.</p>
+    </div>
+  );
+}
